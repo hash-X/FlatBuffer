f90570ff63584cca9e33a4f12c00a72536a598ea
zhangminglei <minglei.l.zhang@intel.com>
2015-10-12 14:11:27 +0800

    message

---
+++ src/org/apache/hadoop/hdfs/Repo.java
@@ -0,0 +1,41 @@
+// automatically generated, do not modify
+
+package src.org.apache.hadoop.hdfs;
+
+import java.nio.*;
+import java.lang.*;
+import java.util.*;
+import com.google.flatbuffers.*;
+
+public class Repo extends Table {
+  public static Repo getRootAsRepo(ByteBuffer _bb) { return getRootAsRepo(_bb, new Repo()); }
+  public static Repo getRootAsRepo(ByteBuffer _bb, Repo obj) { _bb.order(ByteOrder.LITTLE_ENDIAN); return (obj.__init(_bb.getInt(_bb.position()) + _bb.position(), _bb)); }
+  public Repo __init(int _i, ByteBuffer _bb) { bb_pos = _i; bb = _bb; return this; }
+
+  public long id() { int o = __offset(4); return o != 0 ? bb.getLong(o + bb_pos) : 0; }
+  public String name() { int o = __offset(6); return o != 0 ? __string(o + bb_pos) : null; }
+  public ByteBuffer nameAsByteBuffer() { return __vector_as_bytebuffer(6, 1); }
+  public User owner() { return owner(new User()); }
+  public User owner(User obj) { int o = __offset(8); return o != 0 ? obj.__init(__indirect(o + bb_pos), bb) : null; }
+
+  public static int createRepo(FlatBufferBuilder builder,
+      long id,
+      int name,
+      int owner) {
+    builder.startObject(3);
+    Repo.addId(builder, id);
+    Repo.addOwner(builder, owner);
+    Repo.addName(builder, name);
+    return Repo.endRepo(builder);
+  }
+
+  public static void startRepo(FlatBufferBuilder builder) { builder.startObject(3); }
+  public static void addId(FlatBufferBuilder builder, long id) { builder.addLong(0, id, 0); }
+  public static void addName(FlatBufferBuilder builder, int nameOffset) { builder.addOffset(1, nameOffset, 0); }
+  public static void addOwner(FlatBufferBuilder builder, int ownerOffset) { builder.addOffset(2, ownerOffset, 0); }
+  public static int endRepo(FlatBufferBuilder builder) {
+    int o = builder.endObject();
+    return o;
+  }
+};
+
--- test/FlatBufferTest.java
+++ test/FlatBufferTest.java
@@ -14,32 +14,30 @@ public class FlatBufferTest {
   private static final int BUFFER_SIZE = 1000;
   static FlatBufferBuilder fbb = null;
   static ByteBuffer byteBuffer = null;
+  static byte GREEN = 1;
 
   /**
-   * construct a flat binary buffer. The generated functions allow you to add objects to this
-   * buffer recursively. So simple.
+   * Construct data and Serialization.
+   *
    */
   static void BufferConstruction() {
     fbb = new FlatBufferBuilder(BUFFER_SIZE);
     int str = fbb.createString("I am a Monster\n");
     Monster.startMonster(fbb);
+
     Monster.addPos(fbb, Vec3.createVec3(fbb, 12.0f, 2.0f, 3.0f));
-    Monster.addHp(fbb, (short) 180);
+    Monster.addHp(fbb, (short)180);
     Monster.addName(fbb, str);
+    Monster.addColor(fbb, GREEN);
+    Monster.addMana(fbb, (short)330);
+
     int mon = Monster.endMonster(fbb);
     Monster.finishMonsterBuffer(fbb, mon);
-  }
-
-  /**
-   * Store or send your buffer somewhere.
-   * All data in this byeBuffer zone. You can get all the data from byteBuffer.
-   */
-  static void StoreOrTransmittedBuffer() {
     byteBuffer = fbb.dataBuffer();
   }
 
   /**
-   * Deserialization Object
+   * Deserialization
    *
    * @param buffer
    */
@@ -48,22 +46,15 @@ public class FlatBufferTest {
     Monster monster = Monster.getRootAsMonster(buffer);
     System.out.println("Monster hp = " + monster.hp() +
         "\nMonster name = " + monster.name() + "\nMonster new position = " + "x : " + monster.pos().x()
-        + " y : " + monster.pos().y() + " z : " + monster.pos().z());
-    System.out.println("You can also get the binary");
-    byte[] bytes = buffer.array();
-    for (int i = bytes.length - 1; i >= 0; i--) {
-      System.out.print(bytes[i] + " ");
-    }
+        + " y : " + monster.pos().y() + " z : " + monster.pos().z() + "\n" + " Monster color = " + monster.color()
+    + "\n" + " Monster Mana = " + monster.mana());
 }
 
   public static void main(String[] args) {
     byte[] bytes = new byte[BUFFER_SIZE];
     ByteBuffer bb = ByteBuffer.wrap(bytes);
-    Monster monster = Monster.getRootAsMonster(bb);
-    for (int i = 0; i < monster.inventoryLength(); i++)
-      monster.inventory(i); // do something here
+
     BufferConstruction();
-    StoreOrTransmittedBuffer();
     GetDataFromBuffer(bb);
   }
 }
